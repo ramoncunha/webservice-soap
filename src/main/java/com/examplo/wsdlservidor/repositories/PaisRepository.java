13047f3fb8d702711ef66602ae9551d6f598e238
Ramon Cunha <ramon.network@hotmail.com>
2020-10-11 23:41:17 -0300

    Criando metodo buscarPorPais e initData

--- src/main/java/com/examplo/wsdlservidor/repositories/PaisRepository.java
+++ src/main/java/com/examplo/wsdlservidor/repositories/PaisRepository.java
@@ -1,7 +1,9 @@
 package com.examplo.wsdlservidor.repositories;
 
+import com.examplo.wsdlservidor.assets.Moeda;
 import com.examplo.wsdlservidor.assets.Pais;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -15,6 +17,23 @@ public class PaisRepository {
     @PostConstruct
     public void initData() {
         Pais brasil = new Pais();
+        brasil.setNome("Brasil");
+        brasil.setCapital("Brasilia");
+        brasil.setMoeda(Moeda.BRL);
+        brasil.setPopulacao(209300000);
+        paises.put(brasil.getNome(), brasil);
+
+        Pais uk = new Pais();
+        uk.setNome("United Kingdom");
+        uk.setCapital("London");
+        uk.setMoeda(Moeda.GBP);
+        uk.setPopulacao(63705000);
+        paises.put(uk.getNome(), uk);
+    }
+
+    public Pais buscarPorPais(String nome) {
+        Assert.notNull(nome, "O país não pode ser nulo.");
+        return paises.get(nome);
     }
 
 }
